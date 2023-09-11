dd566c6806f74dd600f3cf258a3af92c14694160
Red Penguin <timmyterskov@gmail.com>
2023-09-11 18:37:39 +0100

    Added `Text` ViewElement

--- src/elements.rs
+++ src/elements.rs
@@ -354,6 +354,46 @@ impl ViewElement for Rect {
 #[deprecated = "Please use `Rect` instead"]
 pub type Box = Rect;
 
+/// Displays text starting at the given position
+pub struct Text<'a> {
+    pub pos: Vec2D,
+    pub content: &'a str,
+    pub modifier: Modifier,
+    _private: (),
+}
+
+impl Text<'_> {
+    pub fn new<'a>(pos: Vec2D, content: &'a str, modifier: Modifier) -> Text<'a> {
+        assert!(!content.contains('\n'));
+
+        Text {
+            pos,
+            content: content,
+            modifier,
+            _private: (),
+        }
+    }
+}
+
+impl ViewElement for Text<'_> {
+    fn active_pixels(&self) -> Vec<Point> {
+        let mut pixels = vec![];
+        for (x, char) in self.content.chars().enumerate() {
+            if char != ' ' {
+                pixels.push(Point::new(
+                    self.pos + Vec2D::new(x as isize, 0),
+                    ColChar {
+                        fill_char: char,
+                        modifier: self.modifier,
+                    },
+                ));
+            }
+        }
+
+        pixels
+    }
+}
+
 /// A `ViewElement` that takes a multi-line string as a parameter, and can be used to put ASCII art, text and other such things on the View
 pub struct Sprite {
     pub pos: Vec2D,
@@ -382,17 +422,10 @@ impl ViewElement for Sprite {
 
         let lines = self.texture.split("\n");
         for (y, line) in lines.enumerate() {
-            for (x, char) in line.chars().enumerate() {
-                if char != ' ' {
-                    pixels.push(Point::new(
-                        self.pos + Vec2D::new(x as isize, y as isize),
-                        ColChar {
-                            fill_char: char,
-                            modifier: self.modifier,
-                        },
-                    ));
-                }
-            }
+            pixels.extend(
+                Text::new(self.pos + Vec2D::new(0, y as isize), line, self.modifier)
+                    .active_pixels(),
+            );
         }
 
         pixels
