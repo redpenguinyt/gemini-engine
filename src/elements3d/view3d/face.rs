9e639647dba8c2d7f5c045e5d2b66bd85affdebb
Red Penguin <timmyterskov@gmail.com>
2023-09-13 15:21:23 +0100

    `Face.v_indexes` corrected to `.v_indices`

--- src/elements3d/view3d/face.rs
+++ src/elements3d/view3d/face.rs
@@ -3,20 +3,20 @@ use crate::elements::view::colchar::ColChar;
 /// A Face contains indices to a mesh's collection of vertices and a fill_char to fill the face. Indices should be arranged in a clockwise order, as if they appear counter-clockwise when rendering they will not be rendered at all (this is how gemini-engine handles backface culling and maximises performance)
 #[derive(Debug, Clone)]
 pub struct Face {
-    pub v_indexes: Vec<usize>,
+    pub v_indices: Vec<usize>,
     pub fill_char: ColChar,
 }
 
 impl Face {
-    pub const fn new(v_indexes: Vec<usize>, fill_char: ColChar) -> Self {
+    pub const fn new(v_indices: Vec<usize>, fill_char: ColChar) -> Self {
         Self {
-            v_indexes,
+            v_indices,
             fill_char,
         }
     }
 
     /// Returns a vector with the elements found at the vertex indices of the given slice
     pub fn index_into<T: Copy>(&self, vertices: &[T]) -> Vec<T> {
-        self.v_indexes.iter().map(|vi| vertices[*vi]).collect()
+        self.v_indices.iter().map(|vi| vertices[*vi]).collect()
     }
 }
