60590997fadc9e0aaba1e08c02c3e7fe99d912ce
redpenguinyt <redpenguin777@yahoo.com>
2024-01-15 20:52:41 +0000

    factored interpolate_floating out of interpolate to avoid conversions

--- src/elements/geometry/triangle.rs
+++ src/elements/geometry/triangle.rs
@@ -35,17 +35,25 @@ impl Triangle {
         [self.pos0, self.pos1, self.pos2]
     }
 
-    /// Draw a pseudo-line between the independent and dependent positions
+    /// Draw a pseudo-line between the independent and dependent positions. Returns rounded values as `isize`s. If you don't want the values rounded, use [`Triangle::interpolate_floating()`]
     pub fn interpolate(i0: isize, d0: f64, i1: isize, d1: f64) -> Vec<isize> {
+        Triangle::interpolate_floating(i0, d0, i1, d1)
+            .iter()
+            .map(|n| n.round() as isize)
+            .collect()
+    }
+
+    /// Draw a pseudo-line between the independent and dependent positions
+    pub fn interpolate_floating(i0: isize, d0: f64, i1: isize, d1: f64) -> Vec<f64> {
         if i0 == i1 {
-            return vec![d0.round() as isize];
+            return vec![d0];
         }
         let mut values = vec![];
 
         let a = (d1 - d0) / (i1 - i0) as f64;
         let mut d = d0;
         for _i in i0..(i1 + 1) {
-            values.push(d.round() as isize);
+            values.push(d);
             d += a;
         }
         values
