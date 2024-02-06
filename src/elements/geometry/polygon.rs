53728207732ce4c017a4c7b66ee61c93cba5eaaa
redpenguinyt <redpenguin777@yahoo.com>
2024-02-06 14:54:06 +0000

    finally an actual triangulation algorithm!

--- src/elements/geometry/polygon.rs
+++ src/elements/geometry/polygon.rs
@@ -1,6 +1,35 @@
 use super::Triangle;
 use crate::elements::view::{utils, ColChar, Pixel, Vec2D, ViewElement};
 
+fn is_left_turn(p0: Vec2D, p1: Vec2D, p2: Vec2D) -> bool {
+    let v1 = p1 - p0;
+    let v2 = p2 - p0;
+    v1.cross(v2) > 0
+}
+
+fn is_ear(vertex: Vec2D, prev_vertex: Vec2D, next_vertex: Vec2D, polygon: &[Vec2D]) -> bool {
+    for i in 0..polygon.len() {
+        let p1 = polygon[i];
+        let p2 = polygon[(i + 1) % polygon.len()];
+        let p3 = polygon[(i + 2) % polygon.len()];
+
+        if p1 != vertex
+            && p2 != vertex
+            && p3 != vertex
+            && is_left_turn(vertex, p1, p2)
+            && is_left_turn(vertex, p2, p3)
+            && is_left_turn(vertex, p3, p1)
+        {
+            let triangle_area = (p1 - p2).cross(p3 - p2).abs();
+            if triangle_area > 0 {
+                let p = (vertex - prev_vertex).cross(next_vertex - prev_vertex);
+                return (p1 - prev_vertex).cross(p1 - next_vertex) > 0 && p > 0;
+            }
+        }
+    }
+    false
+}
+
 /// The `Polygon` takes a vec of [`Vec2D`]s and returns a polygon with those vertices when blit to a [`View`](super::super::View)
 pub struct Polygon {
     /// The points that make up the polygon
@@ -16,15 +45,51 @@ impl Polygon {
         Self { points, fill_char }
     }
 
-    /// Split a polygon up into triangles. Returns a vec of coordinate sets for each triangle
+    /// Split a polygon up into triangles using the ear cutting algorithm. Returns a vec of coordinate sets for each triangle
     #[must_use]
     pub fn triangulate(vertices: &[Vec2D]) -> Vec<[Vec2D; 3]> {
-        // TODO: add an actual triangulation algorithm here!
-        let mut points = vec![];
-        for fi in 1..(vertices.len() - 1) {
-            points.push([vertices[0], vertices[fi], vertices[fi + 1]]);
+        let mut triangles = Vec::new();
+        let n = vertices.len();
+
+        if n < 3 {
+            return triangles;
         }
-        points
+
+        let mut remaining_vertices = vertices.to_vec();
+
+        while remaining_vertices.len() > 3 {
+            let mut ear_index = 0;
+            for i in 0..remaining_vertices.len() {
+                let prev_index = (i + remaining_vertices.len() - 1) % remaining_vertices.len();
+                let next_index = (i + 1) % remaining_vertices.len();
+
+                let vertex = remaining_vertices[i];
+                let prev_vertex = remaining_vertices[prev_index];
+                let next_vertex = remaining_vertices[next_index];
+
+                if is_ear(vertex, prev_vertex, next_vertex, &remaining_vertices) {
+                    ear_index = i;
+                    break;
+                }
+            }
+
+            let ear_vertex = remaining_vertices[ear_index];
+            let prev_index = (ear_index + remaining_vertices.len() - 1) % remaining_vertices.len();
+            let next_index = (ear_index + 1) % remaining_vertices.len();
+            let prev_vertex = remaining_vertices[prev_index];
+            let next_vertex = remaining_vertices[next_index];
+
+            triangles.push([prev_vertex, ear_vertex, next_vertex]);
+            remaining_vertices.remove(ear_index);
+        }
+
+        triangles.push([
+            remaining_vertices[0],
+            remaining_vertices[1],
+            remaining_vertices[2],
+        ]);
+
+        triangles
     }
 
     /// Draw a polygon from points. Only supports convex polygons as of now
